163bee496286982ad692bf9a63ed9f39620d6a11
Harun3821 <harun4382183@gmail.com>
2024-03-23 15:43:15 +0600

    first commit disply data show

--- src/component/FeatureJobs/FeatureJobs.jsx
+++ src/component/FeatureJobs/FeatureJobs.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
-
+import Job from "../Job/Job";
 
 
 
 const FeatureJobs = () => {
 
     const [jobs, setJobs] = useState([]);
+
+    //  thes is not the best way to load show all  data 
+
+    const [datalength, setDatalength] = useState(4)
 
 
     useEffect(()=>{
@@ -19,6 +23,15 @@ const FeatureJobs = () => {
             <div className="text-center">
                 <h2 className="text-4xl">Features Jobs: {jobs.length}</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ex totam! Explicabo sapiente sequi ducimus!</p>
+            </div>
+            <div className="grid grid-cols-2 gap-6">
+                {
+                    jobs.slice(0, datalength).map(job => <Job key={job.id} job={job}></Job>)
+                }
+            </div>
+
+            <div className={datalength === jobs.length ? 'hidden' : ''}>
+                <button  onClick={() => setDatalength(jobs.length)}  className="btn btn-primary">Show all jobes</button>
             </div>
         </div>
     );
---
+++ src/component/Job/Job.jsx
@@ -0,0 +1,34 @@
+
+
+import { IoLocationOutline } from "react-icons/io5";
+import { HiOutlineCurrencyDollar } from "react-icons/hi";
+
+const Job = ({job}) => {
+
+    const {logo, job_title, company_name, remote_or_onsite, location, job_type, salary} = job;
+
+    return (
+    <div className="card card-compact  bg-base-100 shadow-xl">
+  <figure><img src={logo} /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{job_title}</h2>
+    <p>{company_name}</p>
+    <div>
+        <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
+        <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4">{job_type}</button>
+    </div>
+
+      <div className="mt-4 flex">
+       <h2 className="flex mr-4"><IoLocationOutline className="text-2xl mr-2"></IoLocationOutline>{location}</h2>
+
+       <h2 className="flex"> <HiOutlineCurrencyDollar className="text-2xl mr-2"></HiOutlineCurrencyDollar>{salary}</h2>
+      </div>
+        <div className="card-actions">
+      <button className="btn btn-primary">VIEW DETAILS</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Job;
